c3a8ba24a7ab44d0f6e6b52039ddcd0d577015a5
Julius Jackson <Julius.Jackson@realart.com>
2018-06-06 10:49:42 -0400

    Second commit

---
+++ src/main/java/Cintas/MavenWebDriverKeywordFramework_2/App.java
@@ -0,0 +1,13 @@
+package Cintas.MavenWebDriverKeywordFramework_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
